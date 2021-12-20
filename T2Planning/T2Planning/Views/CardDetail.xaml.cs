e123ba706e42712935d5b24c31765911c11a4794
Nguyễn Trí <19522389@gm.uit.edu.vn>
2021-12-20 16:10:51 +0700

    20122021

--- T2Planning/T2Planning/Views/CardDetail.xaml.cs
+++ T2Planning/T2Planning/Views/CardDetail.xaml.cs
@@ -147,5 +147,67 @@ namespace T2Planning.Views
             }
         }
 
+        private void addCard()
+        {
+            cardDeadline = deadlineDay.Date.Add(deadlineTime.Time);
+
+            if (card.cardName != cardName_entry.Text)
+            {
+                card.cardName = cardName_entry.Text;
+            }
+
+            if (card.cardDescription != cardDescription_entry.Text)
+            {
+                card.cardDescription = cardDescription_entry.Text;
+            }
+            cardDeadline = deadlineDay.Date.Add(deadlineTime.Time);
+            if (card.cardDeadline != cardDeadline)
+            {
+                card.cardDeadline = cardDeadline;
+            }
+            
+
+            if (database.AddNewCard(card))
+            {
+                DisplayAlert("Tạo the", "Tạo the thành công\n"
+                        + "name: " + card.cardName 
+                        + "\ndes: " + card.cardDescription.ToString()
+                        + "\ntable: " + card.tableId.ToString()
+                        + "\nlistcard: " + card.listCardId.ToString()
+                        + "\nDate: " + card.cardDeadline.ToString(), "OK");
+                Navigation.PopAsync();
+            }
+            else
+            {
+                DisplayAlert("Tạo the", "tạo the thất bại", "OK");
+            }
+        }
+        bool checknull()
+        {
+            if (string.IsNullOrWhiteSpace(cardName_entry.Text) || tableId == null || listCardId == null || cardDeadline == null)
+            {
+                return true;
+            }
+            return false;
+        }
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            if (checknull())
+            {
+                DisplayAlert("Tạo the", "Vui long dien day du thong tin", "OK");
+            }
+            else
+            {
+                addCard();
+            }
+        }
+
+        private void update_Clicked(object sender, EventArgs e)
+        {
+            tablechoose.IsEnabled = true;
+            listchoose.IsEnabled = true;
+            cardName_entry.IsReadOnly = false;
+            cardDescription_entry.IsReadOnly = false;
+        }
     }
 }
